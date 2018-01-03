8bf50823f2b4217798bf843ae5500fdcf35e15b3
Julian Scharf <jscharf@thoughtworks.com>
2018-01-03 12:10:10 +1000

    basic functionality of the subtitle player is working

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const { parse, stringify, resync, toMS, toSrtTime } = require('subtitle');
 const fs = require('fs');
 
+const movieTime = 17084; //milliseconds into the movie
+const offset = Date.now() - movieTime;
 
 const myString = fs.readFileSync('test.srt', 'utf8');
 const mySubs = parse(myString);
 console.log(mySubs);
 
-console.log(myString);
+console.log(offset);
+
+mySubs.forEach((subtitle) => {
+    if (subtitle.start >= movieTime) {
+        setTimeout(() => {
+            console.log(subtitle.text);
+            console.log(Date.now() - offset);
+            
+        }, subtitle.start - movieTime);
+        setTimeout(() => {
+            console.log('');
+        }, subtitle.end - movieTime);
+    }   
+});
